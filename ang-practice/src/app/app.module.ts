5cf7ad4100842859e48c5963632191848fc7e05d
Prasanna Vaidya <vaidya.prasanna@gmail.com>
2024-03-22 18:05:38 +0530

    commit with product and about component 22 March 2024

--- ang-practice/src/app/app.module.ts
+++ ang-practice/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeCompComponent } from './home-comp/home-comp.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeCompComponent
+    HomeCompComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
